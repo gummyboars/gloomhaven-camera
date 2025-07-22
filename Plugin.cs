925a749a54302642f659aab457cf036a5dc815c1
Ozan Demirlioglu <59465487+fingoldfish@users.noreply.github.com>
2025-07-21 23:00:50 -0700

    hello world that patches OnMapLoaded

--- Plugin.cs
+++ Plugin.cs
@@ -1,17 +1,42 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+using MapRuleLibrary.State;
 
 namespace BugFixes;
 
-[BepInPlugin("com.gummyboars.gloomhaven.bugfixes", "Bug Fixes", "0.0.1")]
-public class Plugin : BaseUnityPlugin
+[BepInPlugin(pluginGUID, pluginName, pluginVersion)]
+public class BugFixPlugin : BaseUnityPlugin
 {
-    internal static new ManualLogSource Logger;
+    const string pluginGUID = "com.gummyboars.gloomhaven.bugfixes";
+    const string pluginName = "Bug Fixes";
+    const string pluginVersion = "0.0.1";
+
+    private readonly Harmony HarmonyInstance = new Harmony(pluginGUID);
+
+    public static ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource(pluginName);
         
     private void Awake()
     {
         // Plugin startup logic
-        Logger = base.Logger;
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        BugFixPlugin.logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        HarmonyInstance.PatchAll(assembly);
+    }
+}
+
+[HarmonyPatch(typeof(CMapState), nameof(CMapState.OnMapLoaded))]
+public static class Patch_Map_OnMapLoaded
+{
+    private static void Prefix(bool isJoiningMPClient)
+    {
+        BugFixPlugin.logger.LogInfo($"OnMapLoaded called with {isJoiningMPClient}");
+    }
+
+    private static void Postfix()
+    {
+        BugFixPlugin.logger.LogInfo("OnMapLoaded done");
     }
 }
