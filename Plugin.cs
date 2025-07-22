795639d2bde3ba8259e23fa2347dab75c24218a5
Ozan Demirlioglu <59465487+fingoldfish@users.noreply.github.com>
2025-07-21 21:52:54 -0700

    initial template for bugfix mod

---
+++ Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace BugFixes;
+
+[BepInPlugin("com.gummyboars.gloomhaven.bugfixes", "Bug Fixes", "0.0.1")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
